bad0af801ff99ec014f153764674fcaa2310c43c
antonyjes <72809149@continental.edu.pe>
2024-05-10 13:38:01 -0500

    register schema

--- schemas/index.ts
+++ schemas/index.ts
@@ -3,4 +3,10 @@ import * as z from "zod";
 export const LoginSchema = z.object({
     email: z.string().email({ message: "Email is required" }),
     password: z.string().min(1, { message: "Password is required" }),
+});
+
+export const RegisterSchema = z.object({
+    name: z.string().min(1, { message: "Name is required" }),
+    email: z.string().email({ message: "Email is required" }),
+    password: z.string().min(4, { message: "Minimum password length is 4" }),
 })
