0fa73cb784ceb0ed8abc858bdd6ae0c505189dbb
antonyjes <72809149@continental.edu.pe>
2024-05-11 12:19:16 -0500

    fix error after login message

--- components/auth/login-form.tsx
+++ components/auth/login-form.tsx
@@ -35,8 +35,9 @@ export const LoginForm = () => {
         startTransition(() => {
             login(values)
                 .then((data) => {
-                    setError(data.error);
-                    setSuccess(data.success);
+                    if (data) {
+                        setError(data.error);
+                    }
                 })
         })
     }
