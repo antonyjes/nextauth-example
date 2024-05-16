eca3ada67892e1e2344a802c6390afb17fccd610
antonyjes <72809149@continental.edu.pe>
2024-05-16 18:05:30 -0500

    reset password schema

--- schemas/index.ts
+++ schemas/index.ts
@@ -9,4 +9,8 @@ export const RegisterSchema = z.object({
     name: z.string().min(1, { message: "Name is required" }),
     email: z.string().email({ message: "Email is required" }),
     password: z.string().min(4, { message: "Minimum password length is 4" }),
+});
+
+export const ResetSchema = z.object({
+    email: z.string().email({ message: "Email is required" }),
 })
