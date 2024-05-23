45f764641a2bafd2bf3cb801bc49375d26effb21
antonyjes <72809149@continental.edu.pe>
2024-05-23 18:01:11 -0500

    fetch session to use in client components instead of useSession

--- hooks/use-current-user.ts
+++ hooks/use-current-user.ts
@@ -1,7 +1,35 @@
-import { useSession } from "next-auth/react"
+import { Session } from "next-auth";
+import { getSession } from "next-auth/react";
+import { usePathname } from "next/navigation";
+import { useCallback, useEffect, useState } from "react"
 
 export const useCurrentUser = () => {
-    const session = useSession();
+    const [session, setSession] = useState<Session | null>(null);
+    const [status, setStatus] = useState("unauthenticated");
+    const pathName = usePathname();
 
-    return session.data?.user;
+    const fetchSession = useCallback(async () => {
+        try {
+            setStatus("loading");
+
+            const sessionData = await getSession();
+
+            if (sessionData) {
+                setSession(sessionData);
+                setStatus("authenticated");
+                return;
+            }
+
+            setStatus("unauthenticated");
+        } catch (error) {
+            setStatus("unauthenticated");
+            setSession(null);
+        }
+    }, []);
+
+    useEffect(() => {
+        fetchSession();
+    }, [fetchSession, pathName]);
+
+    return { session, status };
 }
