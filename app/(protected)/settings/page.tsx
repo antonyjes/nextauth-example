43e0e4ac5f72677f3dbd68a01176b4aafd4488eb
antonyjes <72809149@continental.edu.pe>
2024-06-03 20:23:44 -0500

    form user settings

--- app/(protected)/settings/page.tsx
+++ app/(protected)/settings/page.tsx
@@ -1,37 +1,93 @@
 "use client";
 
+import * as z from "zod";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
 import { settings } from "@/actions/settings";
 import { Button } from "@/components/ui/button";
+import { SettingsSchema } from "@/schemas";
 import { Card, CardContent, CardHeader } from "@/components/ui/card";
-import { useTransition } from "react";
+import { useEffect, useState, useTransition } from "react";
 import { useSession } from "next-auth/react";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
 
 const SettingsPage = () => {
+  const { data: session } = useSession();
+  const [defaultValues, setDefaultValues] = useState({ name: "" });
+  const [error, setError] = useState<string | undefined>();
+  const [success, setSuccess] = useState<string | undefined>();
   const { update } = useSession();
   const [isPending, startTransition] = useTransition();
 
-  const onClick = () => {
-    startTransition(() => {
+  useEffect(() => {
+    if (session) {
+      setDefaultValues({ name: session.user?.name ?? "" });
+    }
+  }, [session]);
+
+  const form = useForm<z.infer<typeof SettingsSchema>>({
+    resolver: zodResolver(SettingsSchema),
+    defaultValues: defaultValues,
+  });
+
+  useEffect(() => {
+    form.reset(defaultValues);
+  }, [defaultValues, form]);
+
+  const onSubmit = (values: z.infer<typeof SettingsSchema>) => {
+    startTransition(async () => {
       try {
-        settings({
-          name: "Changed Name",
-        })
-        update();
+        const data = await settings(values);
+        if (data.error) {
+          setError(data.error);
+        }
+
+        if (data.success) {
+          update();
+          setSuccess(data.success);
+        }
       } catch (error) {
         console.log("Error: ", error);
       }
-    })
-  }
+    });
+  };
 
   return (
     <Card className="w-[600px]">
       <CardHeader>
-        <p className="text-2xl font-semibold text-center">
-          ⚙ Settings
-        </p>
+        <p className="text-2xl font-semibold text-center">⚙ Settings</p>
       </CardHeader>
       <CardContent>
-        <Button disabled={isPending} onClick={onClick}>Update name</Button>
+        <Form {...form}>
+          <form className="space-y-6" onSubmit={form.handleSubmit(onSubmit)}>
+            <div className="space-y-4">
+              <FormField
+                disabled={isPending}
+                control={form.control}
+                name="name"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Name</FormLabel>
+                    <FormControl>
+                      <Input
+                        {...field}
+                        placeholder="John Doe"
+                      />
+                    </FormControl>
+                  </FormItem>
+                )}
+              />
+            </div>
+            <Button disabled={isPending} type="submit">Save</Button>
+          </form>
+        </Form>
       </CardContent>
     </Card>
   );
