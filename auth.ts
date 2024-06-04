9be737cb8edfc07308bae4b4bb64cbcf0e429406
antonyjes <72809149@continental.edu.pe>
2024-06-03 20:23:27 -0500

    verify if the session is by oauth

--- auth.ts
+++ auth.ts
@@ -6,6 +6,7 @@ import { db } from "@/lib/db"
 import authConfig from "@/auth.config"
 import { UserRole } from "@prisma/client"
 import { getTwoFactorConfirmationByUserId } from "./data/two-factor-confirmation"
+import { getAccountByUserId } from "@/data/account"
  
 export const { auth, handlers, signIn, signOut } = NextAuth({
   pages: {
@@ -69,6 +70,7 @@ export const { auth, handlers, signIn, signOut } = NextAuth({
       if (session.user) {
         session.user.name = token.name as string;
         session.user.email = token.email as string;
+        session.user.isOAuth = token.isOAuth as boolean;
       }
 
       return session;
@@ -80,6 +82,9 @@ export const { auth, handlers, signIn, signOut } = NextAuth({
 
       if (!existingUser) return token;
 
+      const existingAccount = await getAccountByUserId(existingUser.id);
+
+      token.isOAuth = !!existingAccount;  // !! convert to boolean
       token.name = existingUser.name;
       token.email = existingUser.email;
       token.role = existingUser.role;
